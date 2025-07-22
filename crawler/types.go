e98786884b06af8c241b560834ed56a6083418ae
andretimm <andretimm2012@gmail.com>
2025-07-22 16:46:39 -0300

    feat: :sparkles: alter table to add new repo info

--- crawler/types.go
+++ crawler/types.go
@@ -12,21 +12,28 @@ type Label struct {
 }
 
 type Issue struct {
-	ID            int64     `json:"id"`
-	NodeID        string    `json:"node_id"`
-	URL           string    `json:"html_url"`
-	RepositoryURL string    `json:"repository_url"`
-	Title         string    `json:"title"`
-	Body          string    `json:"body"`
-	User          User      `json:"user"`
-	State         string    `json:"state"`
-	Labels        []Label   `json:"labels"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
-	ProjectName   string    `json:"-"`
-	Language      string    `json:"-"`
+	ID            int64      `json:"id"`
+	NodeID        string     `json:"node_id"`
+	URL           string     `json:"html_url"`
+	RepositoryURL string     `json:"repository_url"`
+	Title         string     `json:"title"`
+	Body          string     `json:"body"`
+	User          User       `json:"user"`
+	State         string     `json:"state"`
+	Labels        []Label    `json:"labels"`
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
+	Repository    Repository `json:"repository"`
+	ProjectName   string     `json:"-"`
+	Language      string     `json:"-"`
 }
 
 type User struct {
 	Login string `json:"login"`
+}
+
+type Repository struct {
+	Description     string `json:"description"`
+	StargazersCount int    `json:"stargazers_count"`
+	ForksCount      int    `json:"forks_count"`
 }
